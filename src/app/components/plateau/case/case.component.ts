4d559c9810947586f6b5e5a0b1937b50a77eb8d7
StefanMartel <stefan.martel@hotmail.fr>
2017-07-07 17:42:33 +0200

    Add files via upload

---
+++ src/app/components/plateau/case/case.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'vic-plateau-case',
+  templateUrl: './case.component.html',
+  styleUrls: ['./case.component.scss']
+})
+
+export class CaseComponent {
+
+
+}
---
+++ src/app/components/plateau/plateau.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'vic-plateau',
+  templateUrl: './plateau.component.html',
+  styleUrls: ['./plateau.component.scss']
+})
+
+export class PlateauComponent {
+
+
+}
